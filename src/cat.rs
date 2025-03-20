12c74eef125db9d109900e27c6ea507795bc444a
Kellan Mebane <kellanmebane@gmail.com>
2025-03-19 20:49:49 -0700

    basically done, skipping a few quirks because they don't seem like fun problems.

--- src/cat.rs
+++ src/cat.rs
@@ -4,6 +4,7 @@
 use std::io;
 use std::io::BufReader;
 use std::io::prelude::*;
+use std::ops::Not;
 
 // main option types:
 // -- `-b` number non-empty output lines
@@ -12,12 +13,13 @@ use std::io::prelude::*;
 // -- `-s` ignore adjacent blank lines
 // -- `-T` display tabs as `^I`
 // -- `-v` display non-printing characters (except tab and lfd)
+// not gonna do -v because I don't really know how it works
 
-// enum LineNumberStyle {
-//     None,
-//     All,
-//     NonEmpty,
-// }
+enum LineNumberStyle {
+    None,
+    All,
+    NonEmpty,
+}
 
 use clap::Parser;
 use patharg::InputArg;
@@ -39,28 +41,75 @@ pub struct CatOptions {
     /// Display TAB characters as `^I`.
     #[arg(short = 'T')]
     display_tab_symbol: bool,
-    /// Display control characters except for LFD and TAB using `^` notation and precede characters that have the high bit set with `M-`.
-    #[arg(short = 'v')]
-    display_unprintables: bool,
+    /// Ignored; for POSIX compatability.
+    #[arg(short = 'u')]
+    do_nothing: bool,
     /// File to operate on, defaults to STDIN.
     #[arg(value_name = "FILE", default_value_t)]
     path: InputArg,
 }
 
 pub fn kitty(cat_options: CatOptions) -> io::Result<()> {
-    let f = BufReader::new(cat_options.path.open()?);
+    let mut f = BufReader::new(cat_options.path.open()?);
 
-    let line_suffix = match cat_options.show_line_end {
-        true => "$",
-        false => "",
-    };
+    let mut line_number_style = LineNumberStyle::None;
+    if cat_options.number_non_blank {
+        line_number_style = LineNumberStyle::NonEmpty;
+    } else if cat_options.number_all_lines {
+        line_number_style = LineNumberStyle::All;
+    }
 
-    for (line_number, line) in f.lines().enumerate() {
-        let prefix = match cat_options.number_all_lines {
-            true => format!("{} ", line_number + 1),
-            false => "".to_string(),
+    let mut line_number: usize = 0;
+    let mut previous_line_blank = false;
+    let mut line = String::new();
+
+    loop {
+        line.clear();
+        if f.read_line(&mut line)? == 0 {
+            break;
+        }
+
+        let trimmed_line = line.trim();
+        if trimmed_line.is_empty() {
+            if previous_line_blank && cat_options.ignore_adjacent_blanks {
+                continue;
+            }
+            previous_line_blank = true;
+        } else {
+            previous_line_blank = false;
+        }
+
+        let prefix = match line_number_style {
+            LineNumberStyle::None => "".to_string(),
+            LineNumberStyle::All => {
+                line_number += 1;
+                format!("\t{} ", line_number)
+            }
+            LineNumberStyle::NonEmpty => {
+                if line.trim().is_empty().not() {
+                    line_number += 1;
+                    format!("\t{} ", line_number)
+                } else {
+                    "".to_string()
+                }
+            }
         };
-        print!("{}{}{}\n", prefix, line?, line_suffix);
+
+        let new_line_pos = match line.rfind('\n') {
+            Some(nlp) => nlp,
+            None => line.len(),
+        };
+
+        if cat_options.show_line_end {
+            line.insert(new_line_pos, '$');
+        }
+
+        line = match cat_options.display_tab_symbol {
+            true => line.replace('\t', "^I"),
+            false => line,
+        };
+
+        print!("{}{}", prefix, line);
     }
 
     Ok(())
